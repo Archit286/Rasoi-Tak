a4b8c27b74ce5f91e328143f0027d07533f23e3b
Archit286 <agupta28_be19@thapar.edu>
2020-09-05 11:54:40 +0530

    Bulid folder

---
+++ react/build/precache-manifest.96d81154e5b04a8fc55734437e1d5e7b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63d827983119a97a33920d019f3c36c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdaf73e23063199639e2",
+    "url": "/static/css/2.c37ac416.chunk.css"
+  },
+  {
+    "revision": "51b213b1dcf44cc165a8",
+    "url": "/static/css/main.972cc6a7.chunk.css"
+  },
+  {
+    "revision": "bdaf73e23063199639e2",
+    "url": "/static/js/2.06b08556.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.06b08556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51b213b1dcf44cc165a8",
+    "url": "/static/js/main.96510255.chunk.js"
+  },
+  {
+    "revision": "0ce40aae65c0e33d7159",
+    "url": "/static/js/runtime-main.405f4c79.js"
+  },
+  {
+    "revision": "e26e87868423d2e71deae44607c9109e",
+    "url": "/static/media/grilled pineapple.e26e8786.JPG"
+  },
+  {
+    "revision": "2ee7b3de22cfcc0a53bb92b8bdd59d02",
+    "url": "/static/media/myimage.2ee7b3de.jpg"
+  },
+  {
+    "revision": "f22ad80d216cd5c693868821c1e7b214",
+    "url": "/static/media/sago shots.f22ad80d.JPG"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "f6478e1ba266e12df5af9e6056a2c42f",
+    "url": "/static/media/waffle.f6478e1b.JPG"
+  }
+]);
